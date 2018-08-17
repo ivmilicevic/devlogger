7f4f822fefd0386427836b454796f7475f55b7e6
Ivan Milicevic <ivmilicevic1@gmail.com>
2018-08-17 14:10:14 +0200

    Created functionality for adding, updating and deleting logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -11,6 +11,8 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+  isNew: boolean = true;
+
 
   constructor(private logService: LogService) { }
 
@@ -21,8 +23,36 @@ export class LogFormComponent implements OnInit {
           this.id = log.id;
           this.text = log.text;
           this.date = log.date;
+          this.isNew = false;
         }
       });
   }
 
+  onSubmit() {
+    if (this.isNew) {
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      }
+
+      this.logService.addLog(newLog);
+    }
+    else {
+      const updatedLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+
+      this.logService.updateLog(updatedLog);
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 }
